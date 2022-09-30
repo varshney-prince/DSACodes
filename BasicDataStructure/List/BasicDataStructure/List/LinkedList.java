ed4218048ef9fc3eba04ffed4c6306ccdfbead10
Prince Varshney <62497419+varshney-prince@users.noreply.github.com>
2022-09-30 18:09:28 +0530

    done

---
+++ BasicDataStructure/List/BasicDataStructure/List/LinkedList.java
@@ -0,0 +1,8 @@
+package BasicDataStructure.List;
+public class LinkedList  {
+
+  
+    SinglyNode head;
+
+   
+}
---
+++ BasicDataStructure/List/BasicDataStructure/List/SinglyNode.java
@@ -0,0 +1,19 @@
+package BasicDataStructure.List;
+class SinglyNode{
+    int val;
+    SinglyNode next;
+
+    SinglyNode(int val){
+        this.val=val;
+        this.next=null;
+    }
+    SinglyNode(){
+        this.val=0;
+        this.next=null;
+    }
+    SinglyNode(int val , SinglyNode address){
+        this.val=0;
+        this.next=null;
+    }
+
+}
