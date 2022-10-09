8b3b66962a9577d6e00de0937549f17c1fe7ea88
Prince Varshney <62497419+varshney-prince@users.noreply.github.com>
2022-10-09 23:47:01 +0530

    sort

--- Algorithms/Recursion/Recursion.java
+++ Algorithms/Recursion/Recursion.java
@@ -15,5 +15,27 @@ public class Recursion {
         }
         return n%10*productOfDigit(n/10);
     }
+
+    public static int reverse(int n){
+        if(n==0){
+            return 0;
+        }
+        return n%10+reverse(n/10)*10;
+    }
+    public static void sortArray(int[] arr,int i){
+        if(i==arr.length-1){
+            return;
+        }
+        int minIndex=i;
+        for(int j=i+1;j<arr.length;j++){
+            if(arr[j]<arr[minIndex]){
+                minIndex=j;
+            }
+        }
+        int temp=arr[i];
+        arr[i]=arr[minIndex];
+        arr[minIndex]=temp;
+        sortArray(arr, i+1);
+    }
     
 }
