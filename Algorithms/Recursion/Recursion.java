d66e3a64390e80b185aae67ce84d40a8521fabbd
Prince Varshney <62497419+varshney-prince@users.noreply.github.com>
2022-10-11 18:21:59 +0530

    subseq

--- Algorithms/Recursion/Recursion.java
+++ Algorithms/Recursion/Recursion.java
@@ -69,5 +69,29 @@ public class Recursion {
         return list;
 
     }
+
+    static void subseq(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        subseq(p + ch, up.substring(1));
+        subseq(p, up.substring(1));
+    }
+
+    static ArrayList<String> subseqRet(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> left = subseqRet(p + ch, up.substring(1));
+        ArrayList<String> right = subseqRet(p, up.substring(1));
+
+        left.addAll(right);
+        return left;
+    }
     
 }
