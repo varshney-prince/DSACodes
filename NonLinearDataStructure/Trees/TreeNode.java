9417a75e2aaae0d7fbbcc8375902607eb1fdba51
Prince Varshney <62497419+varshney-prince@users.noreply.github.com>
2022-10-12 23:11:06 +0530

    add TreeNode

---
+++ NonLinearDataStructure/Trees/TreeNode.java
@@ -0,0 +1,14 @@
+package NonLinearDataStructure.Trees;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    
+    void TreeNode(int val){
+        this.val=val;
+        this.left=null;
+        this.right=null;
+    }
+    
+}
