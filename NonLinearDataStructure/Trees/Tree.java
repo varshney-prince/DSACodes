ea1c7041fbbccd89d0b9236c6f07dc7c7f1ba8b9
Prince Varshney <62497419+varshney-prince@users.noreply.github.com>
2022-10-13 16:26:51 +0530

    tree

--- NonLinearDataStructure/Trees/Tree.java
+++ NonLinearDataStructure/Trees/Tree.java
@@ -1,6 +1,6 @@
 package NonLinearDataStructure.Trees;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Tree {
     TreeNode root;
@@ -38,6 +38,57 @@ public class Tree {
 
     public void display(){
         display(root);
+    }
+
+    public int size(){
+        int s=size( root );
+        return s;
+    }
+
+    private int size(TreeNode t){
+        if(t==null){
+            return 0;
+        }
+
+        int leftCount=size(t.left);
+        int rightCount=size(t.right);
+        int totalCount=leftCount+rightCount+1;
+        return totalCount;
+
+    }
+          
+    public int sum(){
+        int s=sum(root);
+        return s;
+    }
+    private int sum(TreeNode t){
+        if(t==null){
+            return 0;
+        }
+
+        int leftSum=sum(t.left);
+        int rightSum=sum(t.right);
+        int totalSum=leftSum+rightSum+t.val;
+        return totalSum;
+
+
+    }
+
+    public int height(){
+        int h=height(root);
+        return h;
+    }
+
+    private int height(TreeNode root2) {
+        if(root2==null){
+            return -1;
+        }
+
+        int leftHeight=height(root2.left);
+        int rightHeight=height(root2.right);
+
+        int toalHeight=Math.max(leftHeight, rightHeight)+1;
+        return toalHeight;
     }
 
     public Tree(){
