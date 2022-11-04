f52a36a267b3d8ee45dabf72c0c0eaf19dbe47ab
Prince Varshney <62497419+varshney-prince@users.noreply.github.com>
2022-11-04 11:59:54 +0530

    largest BST

--- NonLinearDataStructure/Trees/BST/BST.java
+++ NonLinearDataStructure/Trees/BST/BST.java
@@ -211,4 +211,43 @@ public class BST {
         inorder.add(root.data);
         getInorder(root.right,inorder);
     }
+
+    class Info{
+        boolean isBST;
+        int size;
+        int min;
+        int max;
+
+        public Info(boolean isBST ,int size,int min , int max){
+            this.isBST=isBST;
+            this.min=min;
+            this.size=size;
+            this.max=max;
+        }
+
+        int maxBST=0;
+        public Info largestBST(Node root){
+            if(root==null){
+                return new Info(true,0,Integer.MAX_VALUE,Integer.MIN_VALUE);
+            }
+
+            Info leftInfo=largestBST(root.left);
+            Info rightInfo=largestBST(root.right);
+            int size=leftInfo.size+rightInfo.size+1;
+            int min = Math.min(root.data,Math.min(leftInfo.min,rightInfo.min));
+            int max = Math.max(root.data, Math.max(leftInfo.max, rightInfo.max));
+
+            if(root.data<=leftInfo.max || root.data >=rightInfo.max){
+                return new Info(false, size, min, max);
+            }
+
+            if(leftInfo.isBST && rightInfo.isBST){
+                maxBST=Math.max(maxBST,size);
+                new Info(true, size, min, max);
+            }
+
+            return new Info(false, size, min, max);
+
+        }
+    }
 }
